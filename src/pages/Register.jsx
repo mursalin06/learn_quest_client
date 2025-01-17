63d35cd5489adb388cfdaf99afa2f431a6255e82
MursalinDevOps <mursalin1768@gmail.com>
2025-01-17 10:52:16 +0600

    react hook form implemented on register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-    const handleRegister = (e) => {
-        e.preventDefault();
-        const form = e.target;
+    // const handleRegister = (e) => {
+    //     e.preventDefault();
+    //     const form = e.target;
 
-        const name = form.name.value;
-        const photoURL = form.photo.value;
-        const email = form.email.value;
-        const password = form.password.value;
-        const userInfo = { name, photoURL, email, password };
-        // console.log(userInfo);
+    //     const name = form.name.value;
+    //     const photoURL = form.photo.value;
+    //     const email = form.email.value;
+    //     const password = form.password.value;
+    //     const userInfo = { name, photoURL, email, password };
+    // }
+    const { register, handleSubmit } = useForm()
+    const onSubmit = (registrationData) => {
+        console.log(registrationData)
     }
     return (
         <div>
@@ -23,13 +27,14 @@ const Register = () => {
                 <div className="hero bg-base-200 min-h-screen">
                     <div className="card bg-base-100 w-full mx-5 md:w-10/12 my-6 lg:w-1/3 shadow-2xl">
                         <h2 className="text-2xl font-bold text-center pt-6">Register</h2>
-                        <form onSubmit={handleRegister} className="card-body">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                             {/* NAME */}
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Enter your name</span>
                                 </label>
                                 <input
+                                    {...register("name")}
                                     type="text"
                                     placeholder="name"
                                     name="name"
@@ -43,6 +48,7 @@ const Register = () => {
                                     <span className="label-text">Email</span>
                                 </label>
                                 <input
+                                    {...register("email")}
                                     type="email"
                                     placeholder="email"
                                     name="email"
@@ -56,6 +62,7 @@ const Register = () => {
                                     <span className="label-text">Photo URL</span>
                                 </label>
                                 <input
+                                    {...register("photoUrl")}
                                     type="text"
                                     placeholder="photo url"
                                     name="photo"
@@ -69,6 +76,7 @@ const Register = () => {
                                     <span className="label-text">Password</span>
                                 </label>
                                 <input
+                                    {...register("password")}
                                     type="password"
                                     placeholder="password"
                                     name="password"
@@ -78,7 +86,7 @@ const Register = () => {
                             </div>
                             <p className="hover:underline">
                                 <Link to="/signIn">
-                                Already have an account ?
+                                    Already have an account ?
                                     <span className="text-blue-700 font-bold underline pl-1">
                                         Sign In
                                     </span>
