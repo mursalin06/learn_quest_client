eacc0c0fe26ab7c2fa047ad47be867c00cdfbed5
MursalinDevOps <mursalin1768@gmail.com>
2025-01-20 13:10:16 +0600

    all user data displayed on admins dashboard

--- src/pages/protectedPages/AdminDashboard/AllUsers.jsx
+++ src/pages/protectedPages/AdminDashboard/AllUsers.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import useAxiosPublic from '../../../hooks/useAxiosPublic';
 import { useQuery } from '@tanstack/react-query';
+import { FaUsers } from 'react-icons/fa6';
 
 const AllUsers = () => {
     const axiosPublic = useAxiosPublic();
@@ -16,7 +17,45 @@ const AllUsers = () => {
 
     return (
         <div>
-            ALL USERS - {usersArr.length}
+            <h3 className='text-2xl md:text-3xl font-bold text-[#825afa] text-center my-2'>ALL USERS - {usersArr.length}</h3>
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+
+                                <th>Image</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            {
+                                usersArr.map((singleUser) => <tr key={singleUser._id}>
+                                    <td>
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img
+                                                    src={singleUser?.photoURL} />
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {singleUser?.name}
+                                    </td>
+                                    <td>{singleUser?.email}</td>
+                                    <th>
+                                        <button className="btn btn-ghost btn-xs"><FaUsers></FaUsers> Make Admin</button>
+                                    </th>
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
