4634528eafef13f8fe9fc68b575aed408bfa8ade
MursalinDevOps <mursalin1768@gmail.com>
2025-02-18 21:23:53 +0600

    alert added on newsletter mail submission

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,26 +1,39 @@
-import mailIcon from '../assets/recruiter/icons8-email-100.png'
+import Swal from 'sweetalert2';
+import mailIcon from '../assets/recruiter/icons8-email-100.png';
+
 const Newsletter = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        Swal.fire({
+            title: "Good job!",
+            text: "You're Subscribed!",
+            icon: "success",
+            confirmButtonColor: "#825afa",
+        });
+        form.reset();
+    };
+
     return (
-        <section className='pt-12 pb-24'>
+        <section className='pt-12 pb-24 px-3'>
             <div className='flex flex-col justify-center items-center border rounded-lg border-[#825afa] md:w-1/2 w-11/12 mx-auto py-16 space-y-5 text-center'>
-                <img src={mailIcon} alt="" />
+                <img src={mailIcon} alt="Email Icon" />
                 <h2 className="text-2xl font-bold md:text-4xl">Subscribe to our Newsletter</h2>
                 <p>Your Weekly Dose of LearnQuest Insights</p>
+
+                {/* Mobile form */}
                 <div className='md:hidden'>
-                    <input type="email" placeholder='enter your email' className='input input-bordered' />
-                    <button className='btn bg-[#825afa] mt-1 text-white'>Subscribe</button>
+                    <form onSubmit={handleSubmit}>
+                        <input type="email" required placeholder='Enter your email' className='input input-bordered' />
+                        <button type="submit" className='btn bg-[#825afa] mt-1 text-white'>Subscribe</button>
+                    </form>
                 </div>
+
+                {/* Desktop form */}
                 <div className='hidden md:block'>
-                <div className="join">
-                    <div>
-                        <div>
-                            <input className="input input-bordered join-item" placeholder="Enter your Email" />
-                        </div>
-                    </div>
-                    <div className="indicator">
-                        <button className="btn bg-[#825afa] text-white join-item">Subscribe</button>
-                    </div>
-                </div>
+                    <form onSubmit={handleSubmit} className="join">
+                        <input type="email" required className="input input-bordered join-item" placeholder="Enter your Email" />
+                        <button type="submit" className="btn bg-[#825afa] text-white join-item">Subscribe</button>
+                    </form>
                 </div>
             </div>
         </section>
